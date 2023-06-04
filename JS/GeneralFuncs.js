d3d7f8c614f9852817c6ec345f93a20df7cecfb7
YogurtSnek <pootissonic1808@gmail.com>
2023-06-04 13:44:59 -0700

    I'm fucking insane with these animations

--- JS/GeneralFuncs.js
+++ JS/GeneralFuncs.js
@@ -1,22 +1,35 @@
+/* ---------------- Interactions ---------------- */
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
 /* ---------------- Variables ---------------- */
+// Get the navbar
 var navbar = document.getElementById("NavBar");
+var bodyContainer = document.getElementsByClassName("BodyContainer");
+var sticker = document.getElementById("NavSticker");
+
+// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-
-/* ---------------- Interactions ---------------- */
-window.onscroll = function() {
-    myFunction()
-};
-   
    
 
 
 /* ---------------- Functions ---------------- */
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+        navbar.classList.add("sticky")
+        bodyContainer[0].classList.add("extraPad");
+        sticker.setAttribute("style","animation: fadeIn 1s linear 0s 1 normal forwards; pointer-events: initial;");
+   
+        
+        
     } else {
-    navbar.classList.remove("sticky");
+        navbar.classList.remove("sticky");
+        bodyContainer[0].classList.remove("extraPad");
+        sticker.setAttribute("style","animation: fadeOut 1s linear 0s 1 normal forwards; pointer-events: none;")
+          
+
     }
 }
